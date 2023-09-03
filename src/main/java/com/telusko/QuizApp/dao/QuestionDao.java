88434377078040691d42b6e83d3fb0158a2261aa
GAGAN V <126658094+gaganvdeveloper@users.noreply.github.com>
2023-09-03 10:33:58 +0530

    QuestionDao.java

--- src/main/java/com/telusko/QuizApp/dao/QuestionDao.java
+++ src/main/java/com/telusko/QuizApp/dao/QuestionDao.java
@@ -1,11 +1,8 @@
 package com.telusko.QuizApp.dao;
-
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
 import com.telusko.QuizApp.model.Question;
 import com.telusko.QuizApp.responsestructure.QuestionRepository;
 
@@ -37,5 +34,4 @@ public class QuestionDao {
 	public List<Question> findRandomQuestions(String category, int numQ) {
 		return repository.findRandomQuestions( category, numQ);
 	}
-	
 }
